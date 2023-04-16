5552db1537d86ce7c1cae71c3281e88018c5fe17
y.wada <wada1257@gmail.com>
2023-04-16 22:30:20 +0900

    add DB

---
+++ lib/supabaseFunctions.ts
@@ -0,0 +1,15 @@
+import { supabase } from './supabase';
+
+export const getAllTodos = async () => {
+  const todos = await supabase.from("todo").select('*');
+  console.log(todos.data);
+  return todos.data;
+};
+
+export const addTodo = async (title: string) => {
+  await supabase.from("todo").insert({ title: title });
+};
+
+export const deleteTodo = async (id: number) => {
+  await supabase.from("todo").delete().eq("id", id);
+};
